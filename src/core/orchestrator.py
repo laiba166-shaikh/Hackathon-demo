772a652d465a32b9c8848afbb3d5a4c38f2cb314
Urooj Fatima <uroojf199@gmail.com>
2025-09-26 20:21:46 +0500

    add config file

--- src/core/orchestrator.py
+++ src/core/orchestrator.py
@@ -2,30 +2,38 @@ import os
 import asyncio
 from agents import Agent, Runner, AsyncOpenAI, OpenAIChatCompletionsModel, set_default_openai_client, set_default_openai_api, set_tracing_disabled
 from dotenv import find_dotenv, load_dotenv
+from setup_config import config
 
-load_dotenv(find_dotenv())
-BASE_URL=os.getenv('BASE_URL')
-API_KEY=os.getenv('GEMINI_API_KEY')
-MODEL='gemini-2.0-flash'
 
-client = AsyncOpenAI(
-    api_key=API_KEY,
-    base_url=BASE_URL
-)
+   
+# load_dotenv(find_dotenv())
 
-model = OpenAIChatCompletionsModel(model=MODEL, openai_client=client)
+# BASE_URL=os.getenv('BASE_URL')
+# API_KEY=os.getenv('GEMINI_API_KEY')
+# MODEL='gemini-2.0-flash'
 
-set_default_openai_api('chat_completions')
-set_default_openai_client(client,use_for_tracing=False)
-set_tracing_disabled(True)
+# client = AsyncOpenAI(
+#     api_key=API_KEY,
+#     base_url=BASE_URL
+# )
+# model = OpenAIChatCompletionsModel(
+#     model=MODEL,
+#     openai_client=client)
+
+# set_default_openai_api('chat_completions')
+# set_default_openai_client(client,use_for_tracing=False)
+# set_tracing_disabled(True)
 
 
 async def ask_agent(userMessage:str):
-    agent = Agent(name='Assistant', model=model, instructions='You are a helpful assistant.')
+    agent = Agent(
+        name='Assistant', 
+        instructions='You are a helpful assistant.',
+    )
 
-    result = await Runner.run(agent, userMessage)
+    result = await Runner.run(agent, userMessage,run_config=config)
     output=result.final_output
     return output
     
-# if __name__ == "__main__":
-#     asyncio.run(ask_agent('Hi'))
+if __name__ == "__main__":
+    asyncio.run(ask_agent())
